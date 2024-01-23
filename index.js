7b4dffe1b9e7a9cd1a6740ff4a115a428dc5ab7d
acastoni <acast698@fiu.edu>
2024-01-22 19:09:39 -0500

    -Intitial Setup for Go App

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry, View, Text, Image, StyleSheet } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Go App</Text>
+      <Image 
+        style={styles.image}
+        source={{uri: 'https://media.giphy.com/media/CJRKhi0sixPav1P6MN/giphy.gif' }} 
+      />
+    </View>
+  );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 50,
+  },
+});
+
+AppRegistry.registerComponent('GoApp', () => App);
+
+export default App;
